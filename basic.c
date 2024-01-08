a692dbdcee21ee3fc41fe1edb58b1522a5eb3af5
James Oladiran <oluwajamie@gmail.com>
2024-01-08 08:52:50 -0800

    James Oladiran initial commit

---
+++ basic.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * greet_julien - Prints a greeting message to Julien.
+ *
+ * Return: None
+ */
+void greet_julien(void)
+{
+	puts("Hello Julien, this is my C code for a Betty Check\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	greet_julien();
+	return (0);
+}
+
